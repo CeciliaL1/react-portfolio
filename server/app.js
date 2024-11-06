fdd8a0ea0a4a765331f33f315e72f67599d9c96f
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-11-06 21:15:57 +0100

    :sparkles: connected to database

--- server/app.js
+++ server/app.js
@@ -3,11 +3,19 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 
 
 const app = express();
+
+const connection = require('./lib/conn.js');
+
+connection.connect(function(err){
+  if(err) throw err
+  else console.log("Uppkopplad till databasen");
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
