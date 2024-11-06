a977d8176bf189df060351cf51f94ce6050ed6ce
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-11-06 21:22:25 +0100

    :sparkles: endpont get projects

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const connection = require('../lib/conn.js')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  connection.connect((err) => {
+    if (err) throw err;
+
+    const query = 'SELECT * FROM projects';
+
+    connection.query(query , (err, result) => {
+      if (err) throw err;
+
+      res.json(result);
+    })
+  })
 });
 
 module.exports = router;
