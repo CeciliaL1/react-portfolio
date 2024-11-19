6e7ce96886d0f09eb55c2dddc2c91e58af46796c
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-11-19 21:28:50 +0100

    :Sparkles: updating base in vite-config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/CeciliaL1/react-portfolio/",
+  base: "/CeciliaL1/react-portfolio/tree/main/client/",
 })
